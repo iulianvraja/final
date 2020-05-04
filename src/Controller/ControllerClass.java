c38982fda587621145312d001cfdc4b5d9969595
Vraja Iulian <iulian_vraja@yahoo.com>
2020-05-04 08:57:46 -0700

    f

---
+++ src/Controller/ControllerClass.java
@@ -0,0 +1,37 @@
+
+package Controller;
+
+import Dao.DB_Product;
+import Model.Product;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+/**
+ *
+ * @author manoj.bardhan
+ *
+ */
+
+@Controller
+
+
+public class ControllerClass extends DB_Product {
+    @RequestMapping(value = "/")
+    public ModelAndView home() {
+   
+        ModelAndView x=new ModelAndView("index");
+      
+        return x;
+    }
+
+
+
+    }
+
